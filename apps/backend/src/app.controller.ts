94ea77e7eaf6ba6d5147cedc258a74b6ea3371d9
pstachula <pawel.stachula.mail@gmail.com>
2022-06-16 15:35:53 +0200

    add local strategy

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
   }
 }
---
+++ apps/backend/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,7 @@
+// access_token
+// - lifespan 15 min
+// - rs256
+
+// refresh_token
+//  - 1 week
+//  - es256
