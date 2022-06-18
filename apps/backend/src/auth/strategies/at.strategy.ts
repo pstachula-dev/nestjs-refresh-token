aad1fb0d47974af5af9f3ed06cebf23d6d03d726
pstachula <pawel.stachula.mail@gmail.com>
2022-06-18 20:49:26 +0200

    cleanup

--- apps/backend/src/auth/strategies/at.strategy.ts
+++ apps/backend/src/auth/strategies/at.strategy.ts
@@ -3,6 +3,13 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
+// access_token
+// - lifespan 15 min
+// - rs256
+// refresh_token
+//  - 1 week
+//  - es256
+
 export type JwtPayload = {
   sub: number;
   email: string;
